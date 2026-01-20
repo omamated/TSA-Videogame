330cde82ccff197a9d50a39765d7b509eb445bc0
omamated <113260145+omamated@users.noreply.github.com>
2026-01-19 18:25:02 -0600

    made button and door

---
+++ game-tsa/Scripts/doorandbutton.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@onready var anim := $Door/AnimationPlayer
+
+func _ready():
+	$AnimationPlayer.play("DoorClosed")
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	$AnimationPlayer.play("DoorOpen")
+	pass # Replace with function body.
