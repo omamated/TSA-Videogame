d7048a4cfa794182e8ffe6f89a045c8b91550242
omamated <113260145+omamated@users.noreply.github.com>
2026-01-24 21:43:18 -0600

    Door_fix

--- game-tsa/Scripts/doorandbutton.gd
+++ game-tsa/Scripts/doorandbutton.gd
@@ -3,7 +3,9 @@ extends Node2D
 @onready var anim := $AnimationPlayer
 @onready var door: Sprite2D = $Door
 @onready var button: Sprite2D = $Button
-
+func _ready() -> void:
+	anim.play("Door_init")
+	anim.play("Arrow")
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 	anim.play("DoorOpen")
 	anim.queue("Button_down")  # Queue the button animation to play after DoorOpen
