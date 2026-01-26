12a878e11708a3a6030fa035b3d66e62386addcf
rupaniayaan786-crypto <rupaniayaan786@gmail.com>
2026-01-25 20:54:20 -0600

    V1.14

--- game-tsa/Scenes/death.gd
+++ game-tsa/Scenes/death.gd
@@ -1,9 +1,6 @@
-extends Area2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
+extends CollisionShape2D
 signal player_died
-const SPEED = 100
-var direction = -1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,16 +8,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += direction * SPEED * delta
+	pass
 
 
-func _on_timer_timeout() -> void:
-	direction *= -1
-	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
-	
-
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_killbrick_handler_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		emit_signal("player_died", body)
+		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+
+func _on_player_died() -> void:
 		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
