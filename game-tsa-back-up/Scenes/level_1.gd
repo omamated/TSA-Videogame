f1ddec7c7de5c7ac1d985a4a9ebecfa191e6f294
rupaniayaan786-crypto <rupaniayaan786@gmail.com>
2026-01-25 19:22:14 -0600

    V1.13

--- game-tsa-back-up/Scenes/level_1.gd
+++ game-tsa-back-up/Scenes/level_1.gd
@@ -5,8 +5,18 @@ extends Node2D
 
 func _ready() -> void:
 	$FadeTransitions/AnimationPlayer.play("Fade_Out")
+	_setup_level()
 
 
 func _on_area_2d_for_breaking_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	animation_player.play("break_floor")
 	pass # Replace with function body.
+	
+func _setup_level() -> void:
+	var enemies = $".".get_node_or_null("EnemyHandler")
+	if enemies:
+		for enemy in enemies.get_children():
+			enemy.player_died.connect(_on_player_died)
+			
+func _on_player_died(body):
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
