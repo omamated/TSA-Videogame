34f00e06e505e1895ecba6c72d08486bf28c8b34
rupaniayaan786-crypto <rupaniayaan786@gmail.com>
2026-01-29 21:35:14 -0600

    sadwasdadfdfdaa

--- game-tsa/Scripts/area_2d.gd
+++ game-tsa/Scripts/area_2d.gd
@@ -1,5 +1,5 @@
-extends CollisionShape2D
-signal player_died
+extends Area2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,12 +9,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+signal player_died
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		get_tree().change_scene_to_file("res://Scenes/level2.tscn")
+		get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
 	pass # Replace with function body.
 
 func _on_player_died() -> void:
-		get_tree().change_scene_to_file("res://Scenes/level2.tscn")
+		get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
